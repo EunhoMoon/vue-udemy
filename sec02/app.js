f9884e2882fb955539fc1caae75769a965bdc1d7
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-01-13 17:57:51 +0900

    sec02 - 데이터 출력

--- sec02/app.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    data: function () {
-        return {
-            courseGoal: "Finish the course and learn Vue!"
-        };
-    }
-});
-
-app.mount("#user-goal");
