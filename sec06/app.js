234ec0372229702607cc8d62586d1e780a98dd90
Janek <eunho.moon@gmail.com>
2023-02-05 23:58:35 +0900

    sec06 - 컴포넌트

---
+++ sec06/app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            friends: [
+                {id: "manuel", name: "Manuel Lorenz", phone: "010 1234 1234", email: "manuel@localhost.com"},
+                {id: "julie", name: "Julie Jones", phone: "010 2222 3333", email: "julie@localhost.com"}
+            ]
+        }
+    }
+});
+
+app.component("friend-contact", {
+    template: `
+        <li>
+            <h2>{{friend.name}}</h2>
+            <button @click="toggleDetails">{{detailsAreVisible ? "Hide" : "show Details"}}</button>
+            <ul v-if="detailsAreVisible">
+                <li><strong>Phone:</strong> {{friend.phone}}</li>
+                <li><strong>Email:</strong> {{friend.email}}</li>
+            </ul>
+        </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {id: "manuel", name: "Manuel Lorenz", phone: "010 1234 1234", email: "manuel@localhost.com"}
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+    }
+});
+
+app.mount("#app");
