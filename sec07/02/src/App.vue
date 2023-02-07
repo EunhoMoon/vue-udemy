18da0a543455440b27a79c2fdf14906b5f8d1a52
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-02-07 14:25:28 +0900

    sec07 - 컴포넌트간 통신

--- sec07/02/src/App.vue
+++ sec07/02/src/App.vue
@@ -3,15 +3,20 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
         :key="friend.id"
+        :id="friend.id"
         :name="friend.name"
         :phone-number="friend.phone"
         :email-address="friend.email"
-        :is-favorite="true"
-      ></friend-contact>
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      >
+      </friend-contact>
     </ul>
   </section>
 </template>
@@ -22,19 +27,41 @@ export default {
     return {
       friends: [
         {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "010 1234 1234",
-          email: "manuel@localhos.com",
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '010 1234 1234',
+          email: 'manuel@localhos.com',
+          isFavorite: true,
         },
         {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "010 2222 3333",
-          email: "julie@localhos.com",
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '010 2222 3333',
+          email: 'julie@localhos.com',
+          isFavorite: false,
         },
       ],
     };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find((friend) => friend.id === friendId);
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId) {
+      console.log(friendId)
+      this.friends = this.friends.filter(friend => friend.id !== friendId);
+    },
   },
 };
 </script>
@@ -43,15 +70,12 @@ export default {
 * {
   box-sizing: border-box;
 }
-
 html {
-  font-family: "Jost", sans-serif;
+  font-family: 'Jost', sans-serif;
 }
-
 body {
   margin: 0;
 }
-
 header {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 3rem auto;
@@ -63,14 +87,13 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -79,14 +102,12 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
   color: #58004d;
   margin: 0 0 1rem 0;
 }
-
 #app button {
   font: inherit;
   cursor: pointer;
@@ -96,11 +117,23 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
-
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
--- sec07/02/src/components/FriendContact.vue
+++ sec07/02/src/components/FriendContact.vue
@@ -1,14 +1,15 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '' }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
-      {{ detailsAreVisible ? "Hide" : "Show Details" }}
+      {{ detailsAreVisible ? 'Hide' : 'Show Details' }}
     </button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone: </strong>{{ phoneNumber }}</li>
       <li><strong>Email: </strong>{{ emailAddress }}</li>
     </ul>
+    <button @click="$emit('delete', this.id)">Delete</button>
   </li>
 </template>
 
@@ -16,6 +17,10 @@
 export default {
   // props: [ "name", "phoneNumber", "emailAddress", "isFavorite"],
   props: {
+    id: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
@@ -37,10 +42,20 @@ export default {
       // },
     },
   },
+  emits: ['toggle-favorite', 'delete'],
+  // emits: {
+  //   'toggle-favorite': function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.log('Id is missing');
+  //       return false;
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsAreVisible: false,
-      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
@@ -48,8 +63,9 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
-    },
+      // this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit('toggle-favorite', this.id);
+    }
   },
 };
 </script>
