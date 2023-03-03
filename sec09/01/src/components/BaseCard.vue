55228f1c6f9cfe15c3f448d487706d6f7b5b2d74
Janek <eunho.moon@gmail.com>
2023-03-04 00:46:28 +0900

    sec09 - slot

--- sec09/01/src/components/BaseCard.vue
+++ sec09/01/src/components/BaseCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <header>
-      <slot name="header"></slot>
+    <header v-if="$slots.header">
+      <slot name="header">
+<!--        <h2>The Default</h2>-->
+      </slot>
     </header>
     <slot></slot>
   </div>
@@ -9,6 +11,9 @@
 
 <script>
 export default {
+  mounted() {
+    console.log(this.$slots.header)
+  }
 }
 </script>
 
@@ -19,5 +24,11 @@ div {
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
